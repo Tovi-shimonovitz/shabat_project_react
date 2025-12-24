4d6cde470e6389c635a688ed2d052b74b8cbd20d
הנדסת תוכנה <huser@sem.local>
2025-12-24 16:53:30 +0200

    yehudit change

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import Tovi from './tovi.jsx'
 import Gitty from './Gitty.jsx'
-
+import Yehudit from './the-most -cute-girl.jsx/Yehudit'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Tovi/>
     <Gitty />
+    <Yehudit/>
+
   </StrictMode>,
 )
