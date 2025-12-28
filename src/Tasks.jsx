bdef6979ce32f7d561d9828430302fdf8f6a936d
הנדסת תוכנה <huser@sem.local>
2025-12-28 16:13:33 +0200

    start task component

---
+++ src/Tasks.jsx
@@ -0,0 +1,5 @@
+function MainTask ()
+{
+
+}
+export default MainTask
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Tovi from './tovi.jsx'
-import Gitty from './Gitty.jsx'
-import Yehudit from './the-most -cute-girl.jsx/Yehudit'
+
+
+
+
+import BasisTask from './BasisTask.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Tovi/>
-    <Gitty />
-    <Yehudit/>
-
+ 
+  
+<BasisTask/>
   </StrictMode>,
 )
