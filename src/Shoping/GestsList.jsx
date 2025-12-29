395faa1a1b599da1fc35188f76532b17573bd391
הנדסת תוכנה <huser@sem.local>
2025-12-29 13:04:59 +0200

    add gest list

---
+++ src/Shoping/GestsList.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+
+exports.GestsList = () => {
+
+    let [list, setList] = useState([]);
+    let [inputValue, setInputValue] = useState('');
+    let keyUp = 1;
+    useEffect(() => {
+  
+      setList(['gift', 'Vomit bag', 'suitCase']);
+  
+    }, []);
+  
+  
+  
+    const remove = (item) => {
+      debugger;
+      list = list.filter(t => t !== item);
+      setList(list);
+    }
+  
+    const addItem = (event) => {
+      event.preventDefault();
+      if (inputValue) {
+        setList([...list, inputValue]);
+        setInputValue('');
+      }
+    }
+  
+    return (
+      <>
+        <ul>
+          {list.map(t => <li key={keyUp++}> {t} <button onClick={() => remove(t)}>remove</button> </li>)}
+        </ul>
+  
+        <form onSubmit={addItem} >
+          <input type="text" id="newItem" value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)} />
+          <button type='submit'>add</button>
+        </form>
+  
+      </>
+    )
+}
