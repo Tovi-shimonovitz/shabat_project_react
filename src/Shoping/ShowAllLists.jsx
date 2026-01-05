4627e9c1c8960837b729070f15a800660aeed3a7
michalHaimovich <michal.haimovich@outlook.com>
2026-01-05 23:12:53 +0200

    add show all lists

---
+++ src/Shoping/ShowAllLists.jsx
@@ -0,0 +1,29 @@
+import { ShowItemList } from "./ShowItemList";
+export const ShowAllLists = () => {
+
+    const data = require("../data/Shoping");
+
+    return (
+        <>
+            <h1>Base list</h1>
+            <ShowItemList list={data.getBaseList} />
+
+            <h1>Visiting list</h1>
+            <ShowItemList list={data.getVisitingList} />
+
+            <h1>Hosting list</h1>
+            <ShowItemList list={data.getHostingList} />
+
+            <h1>Friday Night Meal</h1>
+            <ShowItemList list={data.getFridayNightList} />
+
+            <h1>Shabbat Morning Meal</h1>
+            <ShowItemList list={data.getShabbatMorningList} />
+
+            <h1>Third Meal</h1>
+            <ShowItemList list={data.getThirdMealList} />
+
+            <button>save all changes</button>
+        </>
+    )
+}
