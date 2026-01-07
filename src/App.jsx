603670e24b69e45a8cba538b545b3e255c0bcb42
הנדסת תוכנה <huser@sem.local>
2026-01-07 10:23:00 +0200

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 
 import { BaseList } from './Shoping/BaseList'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import BasisTask from './BasisTask';
 
 function App() {
 
   return (
     <>
     <header>
-     <a href="">cooking</a>
-      <a href="">home tasks</a>
-      <a href="">shoping</a>
+     <Router>
+            <Link to="/shopping">shopping</Link>
+            <Link to="/task">home tasks</Link>
+            <Link to="/cooking">cooking</Link>
+            <Switch>
+                <Route path="/shopping">
+                    <BaseList />
+                </Route>
+                <Route path="/task">
+                    <BasisTask/>
+                </Route>
+                <Route path="/cooking">
+                    
+                </Route>
+            </Switch>
+        </Router>
+      
     </header>
      <h1>Preparations for Shabbat</h1>
 
+
+     
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Shabbat from './homePage.jsx'
+import BasisTask from './BasisTask.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    
     <Shabbat/>
- 
+
 
   </StrictMode>,
 )
