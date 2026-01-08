711a78b43255c161400dd7406b1444ababee08d8
Gitty <gitty7426@gmail.com>
2026-01-08 22:41:50 +0200

    add edit option

--- src/Cooking/Food.jsx
+++ src/Cooking/Food.jsx
@@ -1,33 +1,55 @@
-export const Food=(props)=> {
-  const { food ,deleteFood ,setFood} = props;
+import React, { useState } from 'react';
+
+export const Food = (props) => {
+  const { food, deleteFood, setFood } = props;
+  const id = food.id;
+  const [name, setName] = useState(food.name);
+  const [prepTime, setPrepTime] = useState(food.prepTime);
+  const [active, setActive] = useState(true)
+  let style = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+      width: '100%'
+
+
+  }
+
+  let styleNone = { display: 'none' }
+
   return (
     <>
       <li>
-         <div style={{
-          display:'flex',
-          flexDirection:'row',
-          alignItems:'center',
+        {/*this div is for showing the food details when I do not edit it*/}
+        <div style={active ? style : styleNone}>
+          <p><strong>Food:</strong> {name}   </p>
+          <p><strong>Preparation time:</strong> {prepTime} minutes </p>
+          <button onClick={() => {
 
-        // border: '1px solid black',
-        // listStyleType: 'none',
-        gap: '20px',
-        // padding: '10px',
-        // margin: '10px',
-    
-         //width: '350px',
-        // height: '40px',
-        // fontSize: '77%'
-      }} className="foo" >
-            <p><strong>Food:</strong> {food.name}   </p>
-            <p><strong>Preparation time:</strong> {food.prepTime} minutes </p>
-            <button onClick={()=>{
+            deleteFood(food).then(data => {
+              setFood(data);
+            })
+          }}>delete</button>
+          <button onClick={(event) => {
+            event.preventDefault();
+            setActive(!active);
+          }}>edit</button>
 
-             deleteFood(food).then(data => {
-                setFood(data);
+        </div>
 
-             })
-            }}>delete</button>
-            <button>edit</button> 
+        {/*this div is for edit it*/}
+        <div style={active ? styleNone : style}>
+          <input type="text" name="nameN" placeholder="name" onChange={(e) => setName(e.target.value)} style={{ width: '30%' }}/>
+          <input type="number" name="prep" placeholder="prepTime" onChange={(e) => setPrepTime(e.target.value)} style={{ width: '30%' }}/>
+          <button onClick={(event) => {
+            event.preventDefault();
+            setActive(!active);
+          }}>save</button>
+          <button onClick={(event) => {
+            event.preventDefault();
+            setActive(!active);
+          }}>cancel</button>
 
         </div>
       </li>
